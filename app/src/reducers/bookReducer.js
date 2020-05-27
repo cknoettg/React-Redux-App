a63eeb9b0cdb98bcf59c64f3b1607c8de82ea61e
cknoettg <cknoettg@yahoo.com>
2020-05-26 22:31:18 -0400

    Fixed reducer

--- app/src/reducers/bookReducer.js
+++ app/src/reducers/bookReducer.js
@@ -5,22 +5,22 @@ const initialState = { books: [], isbns: [], fetchingBooks: false, error: "" };
 export const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_BOOKS:
-      return Object.assign({}, state, { fetchingBooks: true });
+      return{...state, fetchingBooks: true };
     case BOOK_FETCH_SUCCESS:
-      return Object.assign({}, state, {
+      return {...state, 
         books: [...state.books, action.payload],       
         fetchingBooks: false
-      });
+      };
     case ISBN_SUCCESS:
-      return Object.assign({}, state, {
+      return {...state,
       isbns: [...state.isbns, action.payload],
       fetchingBooks: false
-      });
+      };
     case BOOK_FETCH_ERROR:
-      return Object.assign({}, state, {
+      return {...state,
         fetchingBooks: false, 
         error: "Error fetching books"
-      });
+      };
     default:
       return state;
   }
